9a803b45d1a55fee1f5c51bb48abf9271f1765c6
Gopi M Tatiraju <deathcoderx@gmail.com>
2020-11-17 18:40:16 +0530

    Update TimeConversion.cpp

--- TimeConversion.cpp
+++ TimeConversion.cpp
@@ -1,23 +1,23 @@
 /*
-	As soon as user uses one of the constructor the other format 
-	will also be created and saved inside class variables.
-	24 hour clock format
+  As soon as user uses one of the constructor the other format 
+  will also be created and saved inside class variables.
+  24 hour clock format
 */
 
 /*
-	--------------------------------------------------
-	|Member		|Type|	Meaning									 |Range|
-	--------------------------------------------------
-	|tm_sec		|int |	seconds after the minute |0-60*|
-	|tm_min		|int |	minutes after the hour	 |0-59 |
-	|tm_hour	|int |	hours since midnight		 |0-23 |
-	|tm_mday	|int |	day of the month				 |1-31 |
-	|tm_mon		|int |	months since January		 |0-11 |
-	|tm_year	|int |	years since 1900				 |		 |
-	|tm_wday	|int |	days since Sunday				 |0-6  |
-	|tm_yday	|int |	days since January 1		 |0-365|
-	|tm_isdst	|int |	Daylight Saving Time flag|	   |
-	--------------------------------------------------
+  ------------------------------------------------------
+  |Member		|Type|	Meaning		 |Range|
+  ------------------------------------------------------
+  |tm_sec	|int |	seconds after the minute |0-60*|
+  |tm_min	|int |	minutes after the hour	 |0-59 |
+  |tm_hour	|int |	hours since midnight	 |0-23 |
+  |tm_mday	|int |	day of the month	 |1-31 |
+  |tm_mon	|int |	months since January	 |0-11 |
+  |tm_year	|int |	years since 1900	 |     |
+  |tm_wday	|int |	days since Sunday	 |0-6  |
+  |tm_yday	|int |	days since January 1	 |0-365|
+  |tm_isdst	|int |	Daylight Saving Time flag|     |
+  ------------------------------------------------------
 */
 
 #include <iostream>
@@ -26,58 +26,59 @@
  
 class DateTimeStamp {
 private:
-	time_t epoch;
-	struct tm time;
+  time_t epoch;
+  struct tm time;
 
 public:
-	// Initialize using day, month, year, hour, min, sec
-	// Converts to Epoch 
-	DateTimeStamp(int day, int month, int yr, int hr, int min, int sec)
-	{
-		time.tm_mday = day;
-		time.tm_mon = month;
-		time.tm_year = yr - 1900;
-		time.tm_hour = hr;
-		time.tm_min = min;
-		time.tm_sec = sec;
+  // Initialize using day, month, year, hour, min, sec
+  // Converts to Epoch 
+  DateTimeStamp(int day, int month, int yr, int hr, int min, int sec)
+  {
+    time.tm_mday = day;
+    time.tm_mon = month;
+    time.tm_year = yr - 1900;
+    time.tm_hour = hr;
+    time.tm_min = min;
+    time.tm_sec = sec;
 		
-		epoch = 0;	// Convert Human Readable to Epoch, till then 0
-	}
+    epoch = 0;	// Convert Human Readable to Epoch, till then 0
+  }
 
-	// Initialization using struct tm
-	// Converts Human Readable to Epoch
-	DateTimeStamp(struct tm dateTimeStamp) : time {dateTimeStamp}
-	{
-		epoch = 0;	// Convert Human Readable to Epoch, till then 0
-	}
+  // Initialization using struct tm
+  // Converts Human Readable to Epoch
+  DateTimeStamp(struct tm dateTimeStamp) : time {dateTimeStamp}
+  {
+    epoch = 0;	// Convert Human Readable to Epoch, till then 0
+  }
 
-	// Initialize using Unix Epoch Format 
-	// param type time_t
-	// Converts to Human Readable Format
-	DateTimeStamp(time_t epochTime) : epoch {epochTime}
-	{
-		// Convert Epoch to Human Readable Form
-		localtime_s(&time, &epoch);
-	}
+  // Initialize using Unix Epoch Format 
+  // param type time_t
+  // Converts to Human Readable Format
+  DateTimeStamp(time_t epochTime) : epoch {epochTime}
+  {
+    // Convert Epoch to Human Readable Form
+    localtime_s(&time, &epoch);
+  }
 
-	void print(std::string format = "default")
-	{
-		if (format == "default")
-		{
-			std::cout << time.tm_mday << "/" << time.tm_mon + 1 << "/" 
-								<< time.tm_year + 1900 << " " << time.tm_hour << ":" 
-								<< time.tm_min << ":" << time.tm_sec << std::endl;
-		}
+  void print(std::string format = "default")
+  {
+    if (format == "default")
+    {
+      std::cout << time.tm_mday << "/" << time.tm_mon + 1 << "/" 
+		<< time.tm_year + 1900 << " " << time.tm_hour << ":" 
+		<< time.tm_min << ":" << time.tm_sec << std::endl;
+    }
 
-		else if (format == "epoch")
-		{
-			std::cout << epoch << std::endl;
-		}
+    else if (format == "epoch")
+    {
+      std::cout << epoch << std::endl;
+    }
 
-		else {
-			std::cout << "Unsupported Format!!!" << std::endl;
-		}
-	}
+    else 
+    {
+      std::cout << "Unsupported Format!!!" << std::endl;
+    }
+  }
 };
 
 int main()
